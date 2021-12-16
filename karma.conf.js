a88da6e78b3d269bc6372dbbee08cbbefd3632ac
Johan-Ortega118 <85240547+Johan-Ortega118@users.noreply.github.com>
2021-12-16 00:09:37 -0600

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/realmadrid-players'),
+      dir: require('path').join(__dirname, './coverage/halo-characters'),
       subdir: '.',
       reporters: [
         { type: 'html' },
