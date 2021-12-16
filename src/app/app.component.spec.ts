98c02f274eba2b743377f785afa9c75c6a669c60
Johan-Ortega118 <85240547+Johan-Ortega118@users.noreply.github.com>
2021-12-16 00:17:24 -0600

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CharacterAPI'`, () => {
+  it(`should have as title 'HaloAPI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CharacterAPI');
+    expect(app.title).toEqual('HaloAPI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('CharacterAPI app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('HaloAPI app is running!');
   });
 });
